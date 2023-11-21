07ca398d975e1b9f2191802b8afc9ae532d17b08
shgmlfla <69201914+shgmlfla@users.noreply.github.com>
2023-11-21 15:23:55 +0900

    mariadb

--- src/main/java/com/example/openapi/Entity/Ulsan.java
+++ src/main/java/com/example/openapi/Entity/Ulsan.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
+
 
 @Entity
 @Getter
